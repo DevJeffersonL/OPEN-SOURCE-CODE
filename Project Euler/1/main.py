c9d78565e552a3c59c80870a32bd56ed489a863e
DevJefferson <88711378+DevJeffersonL@users.noreply.github.com>
2022-03-31 09:12:42 +0530

    18

--- Project Euler/1/main.py
+++ Project Euler/1/main.py
@@ -1,12 +1,8 @@
-#Declaring Global variable 
-import os 
-sum=0
 n=1000
-count=1
-def func():
-	while count<n:
-		if(count%3==0 or count%5==0):
-			sum+=count
-func()
-print(sum)
-os.system('pause >nul')
+count=0
+s=0
+while count<n:
+	if(count%3==0 or count%5==0):
+		s+=count
+	count+=1
+print(s)
