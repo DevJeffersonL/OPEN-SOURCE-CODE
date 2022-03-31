4eaeb1fcc03002079eeae0828bd1f42ab52547e4
DevJefferson <88711378+DevJeffersonL@users.noreply.github.com>
2022-03-31 09:16:02 +0530

    19

--- Project Euler/1/main.py
+++ Project Euler/1/main.py
@@ -1,8 +1,8 @@
-n=1000
-count=0
-s=0
-while count<n:
-	if(count%3==0 or count%5==0):
-		s+=count
-	count+=1
-print(s)
+def func():
+	count=0;n=1000; s=0 
+	while count<n:
+		if(count%3==0 or count%5==0):
+			s+=count
+		count+=1
+	print(s)
+func()
